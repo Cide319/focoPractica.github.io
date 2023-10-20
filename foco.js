9647640f89ac75a3e0327417bec57640e32dfdd2
Cide319 <147774898+Cide319@users.noreply.github.com>
2023-10-20 17:35:23 -0600

    Update foco.js

--- foco.js
+++ foco.js
@@ -3,23 +3,24 @@ window.addEventListener('load', function(){
 });
 
 function foco(){
-    document.querySelector('#body').classList.add('oscuridad');
+    document.querySelector('body').classList.add('oscuridad');
     const titulo = document.createElement('h1');
     titulo.classList.add('titulo');
     const anio = new Date().getFullYear();
     titulo.innerText = `Desarrollo de sistemas weba: ${anio}`
     document.body.appendChild(titulo);
+    
     const img = document.createElement('img');
     img.classList.add('foco');
-    img.setAttribute('src', foco-apagado.jpg)
+    img.setAttribute('src', '/foco-apagado.jpg')
 
     img.addEventListener('click', () =>{
         if(img.src.indexOf('apagado') > 0){
-            img.setAttribute('src', foco-prendido.jpg);
+            img.setAttribute('src', '/foco-prendido.jpg');
             document.body.classList.remove('oscuridad');
             document.body.classList.add('luz');
         } else{
-            img.setAttribute('src', foco-apagado.jpg);
+            img.setAttribute('src', '/foco-apagado.jpg');
             document.body.classList.remove('luz');
             document.body.classList.add('oscuridad');
         }
